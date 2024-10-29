7bbc86d123b0f90e4b064a3f75e53ef83e71b660
Saymon262 <saymon_botelho123@outlook.com>
2024-10-28 22:36:07 -0300

    Chess Layer and Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,15 @@ package application;
 
 import board.Board;
 import board.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8 ,8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getpieces());
+		
 		
 	}
 
