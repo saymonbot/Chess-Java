65a42a8f12a4b0004be1811f549030e3bde7b77b
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-08-01 16:10:53 -0300

    Update Bishop.java

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -61,8 +61,7 @@ public class Bishop extends ChessPiece{
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-			
+		
 		return mat;
 	}
-
 }
